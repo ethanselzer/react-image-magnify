feb913b5acb152e2614acfb80e4f6f5c6be82f9c
Ethan Selzer <ethanselzer@gmail.com>
2018-10-20 11:22:56 -0700

    Example: Local Package Import - Support Windows

--- example/src/components/ResponsiveImages.js
+++ example/src/components/ResponsiveImages.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactImageMagnify from '../pkg-lnk/ReactImageMagnify';
+import ReactImageMagnify from 'ReactImageMagnify';
 import SpacedSpan from '../components/SpacedSpan';
 
 import '../styles/app.css';
