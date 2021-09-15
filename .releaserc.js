17e3d9127a15b3e48ae62770d82ee81484340a1a
Brandon Webb <Brandon.Webb@vistaprint.com>
2021-09-15 13:43:05 -0400

    fix: push updated package.json back to master

--- .releaserc.js
+++ .releaserc.js
@@ -14,5 +14,9 @@ module.exports = {
         ["@semantic-release/github", {
             "assets": []
         }],
+        ["@semantic-release/git", {
+            "assets": ["package.json"],
+            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+        }]
     ],
 };
